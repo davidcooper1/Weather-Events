86ec5daf986ea656b1ce8c5d2a2b70ce109a6242
davidcooper1 <davidpcooper1@gmail.com>
2018-12-04 03:25:58 -0600

    Added ability to download CSV files from NOAA's NWS Storm Events Database.

---
+++ WeatherEvents/js/data.js
@@ -0,0 +1,56 @@
+var Client = require("ftp");
+var fs = require("fs");
+var zlib = require("zlib");
+
+var c = new Client();
+c.on("ready", () => {
+	c.cwd("pub/data/swdi/stormevents/csvfiles/", (err, cwd) => {
+		if (err) throw err;
+	});
+	c.list((err, list) => {
+		if (err) throw err;
+		fileList = list;
+		getLocationAndFatalityFiles(c, list);
+		c.end();
+	});
+});
+
+c.connect({
+	host: "ftp.ncdc.noaa.gov"
+});
+
+function getLocationAndFatalityFiles(c, list) {
+	for (var i = 0; i < list.length; i++) {
+		var fileName = list[i].name;
+		if (fileName.startsWith("StormEvents_locations") || fileName.startsWith("StormEvents_fatalities")) {
+			let fn = fileName;
+			c.get(fn, (err, stream) => {
+				if (err) throw err;
+				var gunzip = zlib.createGunzip();
+				var toDelay = null;
+				function setDelay() {
+					toDelay = setTimeout(() => {
+						stream.end();
+					}, 200);
+				}
+				stream.once("close", () => { console.log(fn + "done.") });
+				stream.on("data", (chunk) => { 
+					clearTimeout(toDelay);
+					setDelay();
+				});
+				stream.pipe(gunzip).pipe(fs.createWriteStream("data/" + fn.substring(0, fn.length-3)));
+				return;
+			});
+		}
+	}
+}
+
+function getDateLastModified(list) {
+	var lastMod = 0;
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].date.getTime() > lastMod) {
+			lastMod = list[i].date.getTime();
+		}
+	}
+	return lastMod;
+}
---
+++ WeatherEvents/main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require("electron")
+
+let win;
+
+function createWindow() {
+	win = new BrowserWindow({
+		minWidth: 600,
+		minHeight: 400,
+		width: 850,
+		height: 650
+	});
+	
+	win.loadFile("index.html");
+	win.setMenu(null);
+	
+	win.webContents.on("before-input-event", (event, input) => {
+		if (input.key == "F12") {
+			if (win.webContents.isDevToolsOpened()) {
+				win.webContents.closeDevTools();
+			} else {
+				win.webContents.openDevTools();
+			}
+		}
+	});
+	
+	win.on("closed", () => {
+		win = null;
+	});
+}
+
+
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+app.on("activate", () => {
+	if (win == null) {
+		createWindow();
+	}
+});
